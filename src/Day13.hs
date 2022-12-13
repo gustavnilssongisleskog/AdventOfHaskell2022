6ecedccae3ffbed2b3e1037e255d75072ddccf51
Gustav Nilsson Gisleskog <gisleskoggustav@gmail.com>
2022-12-13 17:56:39 +0100

    made Eq Packet work too

--- src/Day13.hs
+++ src/Day13.hs
@@ -3,25 +3,30 @@ import Data.Char(isDigit)
 import Data.List(sort)
 import Utils(partitionPieceLength, listToTuple)
 
-data Packet = List [Packet] | Number Int deriving (Read, Show, Eq)
+data Packet = List [Packet] | Number Int deriving (Read, Show)
 
 path = "input/13.in"
 
---func = readFile path >>= (print . sum . map fst . filter (uncurry (<) . snd) . zip [1..] . map (listToTuple . map readPacket . take 2) . partitionPieceLength 3 . lines)
-func = readFile path >>= (print . product . map fst . filter (\(_,num) -> num == List [List [Number 2]] || num == List [List [Number 6]]) . zip [1..] . sort . concatMap (map readPacket . take 2) . partitionPieceLength 3 . lines . (++ "\n\n[[2]]\n[[6]]"))
+func = readFile path >>= (print . sum . map fst . filter (uncurry (<) . snd) . zip [1..] . map (listToTuple . map readPacket . take 2) . partitionPieceLength 3 . lines)
+--func = readFile path >>= (print . product . map fst . filter (\(_,num) -> num == List [List [Number 2]] || num == List [List [Number 6]]) . zip [1..] . sort . concatMap (map readPacket . take 2) . partitionPieceLength 3 . lines . (++ "\n\n[[2]]\n[[6]]"))
+
+instance Eq Packet where
+    (==) = ((==EQ) .) . comparePackets
 
 instance Ord Packet where
-    compare (List as) (List bs) = compareLists as bs
-    compare (List as) (Number b) = compareLists as [Number b]
-    compare (Number a) (List bs) = compareLists [Number a] bs
-    compare (Number a) (Number b) = compare a b
+    compare = comparePackets
+
+comparePackets :: Packet -> Packet -> Ordering
+comparePackets (List as) (List bs) = compareLists as bs
+comparePackets (List as) (Number b) = compareLists as [Number b]
+comparePackets (Number a) (List bs) = compareLists [Number a] bs
+comparePackets (Number a) (Number b) = compare a b
 
 compareLists :: [Packet] -> [Packet] -> Ordering
 compareLists [] [] = EQ
 compareLists [] _ = LT
 compareLists _ [] = GT
-compareLists (a:as) (b:bs) = if comparison == EQ then compareLists as bs else comparison where
-    comparison = compare a b
+compareLists (a:as) (b:bs) = if a == b then compareLists as bs else compare a b
 
 readPacket :: String -> Packet
 readPacket = read . addNumber . concatMap ((\x -> if x == "[" then "List [" else x) . return) where
